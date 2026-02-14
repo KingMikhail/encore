b476a551275253f4075ca020f1c4c10b8c419142
Mikhail Andrei <andreimikhail1987@gmail.com>
2026-02-14 20:46:19 +0800

    Update verify.sh

--- module/verify.sh
+++ module/verify.sh
@@ -18,11 +18,11 @@ TMPDIR_FOR_VERIFY="$TMPDIR/.vunzip"
 mkdir "$TMPDIR_FOR_VERIFY"
 
 abort_verify() {
-	ui_print "*********************************************************"
-	ui_print "! $1"
-	ui_print "! Installation aborted. The module may be corrupted."
-	ui_print "! Please re-download and try again."
-	abort "*********************************************************"
+	ui_print "*****"
+	ui_print "$1"
+	ui_print "Installation Aborted, Module may be Corrupted"
+	ui_print "Re-Download and Try again"
+	abort "*****"
 }
 
 # extract <zip> <file> <target dir>
@@ -35,23 +35,23 @@ extract() {
 	hash_path="$TMPDIR_FOR_VERIFY/$file.sha256"
 
 	unzip -o "$zip" "$file" -d "$dir" >&2
-	[ -f "$file_path" ] || abort_verify "$file does not exists"
+	[ -f "$file_path" ] || abort_verify "$file Does not Exists"
 
 	unzip -o "$zip" "$file.sha256" -d "$TMPDIR_FOR_VERIFY" >&2
-	[ -f "$hash_path" ] || abort_verify "Missing checksum for $file"
+	[ -f "$hash_path" ] || abort_verify "Missing Checksum for $file"
 
 	(echo "$(cat "$hash_path")  $file_path" | sha256sum -c -s -) || abort_verify "Checksum mismatch for $file"
-	# ui_print "- Verified $file" >&1
+	# ui_print "Verified $file" >&1
 }
 
 file="META-INF/com/google/android/update-binary"
 file_path="$TMPDIR_FOR_VERIFY/$file"
 hash_path="$file_path.sha256"
 unzip -o "$ZIPFILE" "META-INF/com/google/android/*" -d "$TMPDIR_FOR_VERIFY" >&2
-[ -f "$file_path" ] || abort_verify "$file does not exists"
+[ -f "$file_path" ] || abort_verify "$file Does not Exists"
 if [ -f "$hash_path" ]; then
-	(echo "$(cat "$hash_path")  $file_path" | sha256sum -c -s -) || abort_verify "Checksum mismatch for $file"
-	# ui_print "- Verified $file" >&1
+	(echo "$(cat "$hash_path")  $file_path" | sha256sum -c -s -) || abort_verify "Checksum Mismatch for $file"
+	# ui_print "Verified $file" >&1
 else
-	ui_print "- Download from Magisk app"
+	ui_print "Download from Magisk App"
 fi
